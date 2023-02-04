31046a9b3dc8a8995a9e076faebe4dcbc3f6683f
Maftuna <98867890+kifo3@users.noreply.github.com>
2023-02-04 16:06:43 +0300

    createRef fiest commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,72 +1,94 @@
 import React from "react";
 
 export default class Form extends React.Component {
-    state = {
-        email: '',
-        isAgree: false
+    constructor() {
+        super();
+        this.state = {
+            card: '',
+            cv: '',
+            name: '',
+        };
+        this.cardRef = React.createRef();
+        this.cvRef = React.createRef();
+        this.nameRef = React.createRef();
     }
 
-    HandleEmail = (e) => {
-        this.setState({email: e.target.value})
+
+    HandleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value}, () => {
+            if(this.state.card.length === 16) {
+                this.cvRef.current.focus();
+            }
+
+            if(this.state.cv.length === 3) {
+                this.nameRef.current.focus();
+            }
+        })
     }
 
-    checkedHandle = (e) => {
-        this.setState({isAgree: e.target.checked})
+    componentDidMount() {
+        this.cardRef.current.focus()
     }
-
-
 
     handleSubmit = () =>{
-        const re = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
-        const isValidEmail = re.test(this.state.email)
-        const isValidChecked = this.state.isAgree;
 
-        if(!isValidEmail) {
-            alert('Your email is not valid')
+        if(!this.state.card) {
+            alert('Please enter card number')
             return
         }
-        if(!isValidChecked){
-            alert('You should accept all termrs & conditions')
+        if(!this.state.cv){
+            alert('CV number invalid')
             return
         }
 
-        this.setState({email: '', isAgree: false})
+        this.setState({card: '', cv: '', name: ''})
 
         alert('Thank you for subscription')
     }
+
+
+
     render() {
-        const {email, isAgree} = this.state
+        const {card, cv, name} = this.state
         return(
             <div>
                 <h1>Form</h1>
 
-                 <form class="col-md-12 ml-3" >
-                     <div class="mb-3 ">
-                         <label class="form-label">Email</label>
+                 <form class="row ml-3" >
+                     <div class="col-md-8 mb-3">
                          <input
                              class="form-control"
                              type='text'
-                             placeholder="Email"
-                             name="email"
-                             value={email}
-                             onBlur={this.validEmail}
-                             onChange={this.HandleEmail}
+                             placeholder="Card Number"
+                             name="card"
+                             value={card}
+                             ref={this.cardRef}
+                             onChange={this.HandleChange}
                          />
                      </div>
-                     <div class="form-check">
-                          <input
-                                class="form-check-input"
-                                type="checkbox"
-                                name="isAgree"
-                                checked={isAgree}
-                                onChange={this.checkedHandle}
-                          />
-                          <label class="form-check-label">
-                            I agree with terms and conditions
-                          </label>
+                     <div class="col-md-4 mb-3 ">
+                         <input
+                             class="form-control"
+                             type='text'
+                             placeholder="CV"
+                             name="cv"
+                             value={cv}
+                             ref={this.cvRef}
+                             onChange={this.HandleChange}
+                         />
                      </div>
-                     <button onClick={this.handleSubmit} type="submit" class="btn btn-primary">Submit</button>
-
+                     <div class="col-md-12 mb-3 ">
+                         <input
+                             class="form-control"
+                             type='text'
+                             placeholder="Name"
+                             name="name"
+                             value={name}
+                             ref={this.nameRef}
+                             onChange={this.HandleChange}
+                         />
+                     </div>
+                      <button onClick={this.handleSubmit} type="submit" class="btn btn-primary">Submit</button>
                  </form>
 
             </div>
